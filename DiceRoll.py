a1050f56af4201f88f16f5637ce8cad38830cd80
U-N-Owen <mshiltsev@gmail.com>
2014-04-16 11:05:43 -0400

    Made the program awesomer than it was, you can't break the program anymore with negative dice, and you can't have less than two dice

--- DiceRoll.py
+++ DiceRoll.py
@@ -4,15 +4,23 @@ import random
 import specialinputcopy as sic
 
 # ----------- Main Program here ----------
+# -----Input
 print "Welcome to U-N-Owen's amazing dice rolling program!"
 print "\n"
 numberofsides = sic.int_input("How many sides does the die you want to roll have? | ")
-numofdice = sic.int_input("How many of these dice would you like to roll?|")
+while numberofsides <=1:
+    numberofsides = sic.int_input("enter a number greater than 2 | ")
 
+numofdice = sic.int_input("How many of these dice would you like to roll?|")
+while numofdice < 0
+mrosu:
+    numofdice = sic.int_input("you can't roll negative dice|")
+
+# -----Output
 numtimesdone = 1
 while numtimesdone <= numofdice:
     dieoutcome = random.randint(1,numberofsides)
     print "Here's the result of the roll |", dieoutcome
     numtimesdone += 1
-print "Finished, above are your", numofdice,"D" + numberofsides + " rolls."
+print "Finished, above are your", str(numofdice),"D" + str(numberofsides) + " rolls."
 
